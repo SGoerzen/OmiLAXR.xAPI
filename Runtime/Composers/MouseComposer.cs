4468105818237608da2a9e1e9ff8c18bc6a916aa
Sergej Görzen <sergej-goerzen@web.de>
2024-10-09 20:37:49 +0200

    remove scroll definitions because they are not pushed in main branch of xapi registry yet

--- Runtime/Composers/MouseComposer.cs
+++ Runtime/Composers/MouseComposer.cs
@@ -31,13 +31,14 @@ namespace OmiLAXR.xAPI.Composers
             });
             tb.OnScrolledWheel.AddHandler((_, args, value) =>
             {
-                var statement = actor.Does(xapi.generic.verbs.scrolled)
-                    .Activity(xapi.generic.activities.mouse, 
-                        xapi.generic.extensions.activity
-                            .mouseButton(args.mouseButton)
-                            .mousePosition(args.mousePosition)
-                            .scrollValue(value));
-                SendStatement(statement);
+                // Todo: add again, when xAPI Registry entries are accepted
+                // var statement = actor.Does(xapi.generic.verbs.scrolled)
+                //     .Activity(xapi.generic.activities.mouse, 
+                //         xapi.generic.extensions.activity
+                //             .mouseButton(args.mouseButton)
+                //             .mousePosition(args.mousePosition)
+                //             .scrollValue(value));
+                // SendStatement(statement);
             });
             tb.OnMousePositionChanged.AddHandler((_, pos) =>
             {
